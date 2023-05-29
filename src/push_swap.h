7f61ae4a6cc38936c6042815c3ac629ea59c41dd
andoitzcp <andoitzcp@gmail.com>
2023-05-29 08:28:58 +0200

    header file created and node struct defined

---
+++ src/push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acampo-p <acampo-p@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/28 22:11:30 by acampo-p          #+#    #+#             */
+/*   Updated: 2023/05/28 22:23:00 by acampo-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP.H
+# define PUSH_SWAP.H
+
+# include "../libft/libft.h"
+
+typedef struct		snode
+{
+	int				i;
+	struct snode	*next;
+}					snode;
+
+#endif
